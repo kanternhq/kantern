b5d6c33b4d7ddca5b04ef30ae451c445cc8437f7
Marius Kimmina <mar.kimmina@gmail.com>
2024-07-21 11:11:42 +0200

    stuff

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 import { AppShell, Burger } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+function Pods() {
+  return (
+    <div>
+      <h1>Pods Page</h1>
+    </div>
+  );
+}
+
+function Services() {
+  return (
+    <div>
+      <h1>Services Page</h1>
+    </div>
+  );
+}
+
+function Deployments() {
+  return (
+    <div>
+      <h1>Deployments Page</h1>
+    </div>
+  );
+}
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
@@ -23,7 +56,12 @@ function App() {
       <Navbar />
 
       <AppShell.Main>
-        <h1>Main</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pods" element={<Pods />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/deployments" element={<Deployments />} />
+        </Routes>
       </AppShell.Main>
     </AppShell>
   );
